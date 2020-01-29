bd782baa395120a605f6c609793171b6315acf6d
ralph2g <ralph2g@outlook.com>
2020-01-29 12:47:03 -0600

    Se arreglan algunos detalles con la sintxis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
 import { routes } from './app.router';
 
 import { AppRoutingModule } from './app-routing.module';
