f4679901fd573c964995172f865b8f2fa77768c1
Moisés <moises@MacBook-Pro-de-Moises.local>
2020-02-10 19:40:59 -0600

    QuitandoGetCurrentPosition

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -37,32 +37,6 @@ export class LocationService {
     }
   }
 
-  public getCurrenPosition(options?:any):Observable<any>{
-    return Observable.create( observer =>{
-      if(window.navigator && window.navigator.geolocation){
-        let watchId = window.navigator.geolocation.watchPosition(
-          (position)=>{
-            observer.next(position);
-          },
-          (error)=>{
-            switch (error.code){
-              case 1:observer.error("You have rejected to your location");break;
-              case 2:observer.error("Unable to determine your location");break;
-              case 3:observer.error("Service timeout has been reached"); break;
-            }
-          },options);
-        // PATCH: Con el timeout nos aseguramos de dejar de obtener la ubicación
-        // en google chrome después de cierto tiempo.
-        let timeout = setTimeout(()=>{
-          console.log("stop watching...");
-          observer.complete();
-          window.navigator.geolocation.clearWatch( watchId );
-        },options.timeout);
-      }else{
-        observer.error("Browser doesn't support location service");
-      }
-    });
-  }
   // Obtiene el estado de una ciudad segun sus coordenadas
   async getState(lat:number, long:number):Promise<any>{
     try{
