4aa42c4cc53db83b37d27bcc931d5097496a0bde
unknown <crispinjesus97@gmail.com>
2020-02-12 20:06:16 -0600

    corregido IMG

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,4 +2,4 @@ import { Routes } from '@angular/router';
 import { NoPageRoutes } from './no-page/no-page.routes';
 import { HomeRoutes } from './home/home.router';
 
-export const routes: Routes = [...HomeRoutes, ...NoPageRoutes];
+export const routes: Routes = [...HomeRoutes];
