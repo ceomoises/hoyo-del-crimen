2d92db1e47d8129a84aca4f0b4e40d095e29653b
Rafael Garcia Garcia <37715000+Ralph2g@users.noreply.github.com>
2020-02-26 21:15:42 -0600

    Se inicializa n componentes de badges de materialize

--- src/app/models/iconsMap.ts
+++ src/app/models/iconsMap.ts
@@ -1,20 +1,20 @@
 export const IconsMap: Array<string> = [
-    "http://localhost:4200/assets/img/subway.svg",
-    "http://localhost:4200/assets/img/subway_d.svg",
-    "http://localhost:4200/assets/img/bus_d.svg",
-    "http://localhost:4200/assets/img/bus.svg",
-    "http://localhost:4200/assets/img/taxi.svg",
-    "http://localhost:4200/assets/img/house.svg",
-    "http://localhost:4200/assets/img/account.svg",
-    "http://localhost:4200/assets/img/store.svg",
-    "http://localhost:4200/assets/img/deliver.svg",
-    "http://localhost:4200/assets/img/deliver_d.svg",
-    "http://localhost:4200/assets/img/crime.svg",
-    "http://localhost:4200/assets/img/crime_d.svg",
-    "http://localhost:4200/assets/img/vehicle.svg",
-    "http://localhost:4200/assets/img/vehicle_d.svg",
-    "http://localhost:4200/assets/img/kidnapping.svg",
-    "http://localhost:4200/assets/img/rape.svg",
-    "http://localhost:4200/assets/img/gun.svg",
+    "http://localhost:4200/assets/img/subway.svg",//TP-M
+    "http://localhost:4200/assets/img/subway_d.svg",//TP-M+
+    "http://localhost:4200/assets/img/bus_d.svg",//TP-B-+
+    "http://localhost:4200/assets/img/bus.svg",//TP-B
+    "http://localhost:4200/assets/img/taxi.svg",//TP-T
+    "http://localhost:4200/assets/img/house.svg",//NA
+    "http://localhost:4200/assets/img/account.svg",//ST
+    "http://localhost:4200/assets/img/store.svg",//NA
+    "http://localhost:4200/assets/img/deliver.svg",//Pendiente
+    "http://localhost:4200/assets/img/deliver_d.svg",//Pendiente
+    "http://localhost:4200/assets/img/crime.svg",//ST
+    "http://localhost:4200/assets/img/crime_d.svg",//ST
+    "http://localhost:4200/assets/img/vehicle.svg",//TP
+    "http://localhost:4200/assets/img/vehicle_d.svg",//TP
+    "http://localhost:4200/assets/img/kidnapping.svg",//NA
+    "http://localhost:4200/assets/img/rape.svg",//NA
+    "http://localhost:4200/assets/img/gun.svg",//
 ]
 
